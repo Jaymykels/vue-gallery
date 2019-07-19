55734654d7b9d4ec799c868c4b36dfd44fdaf1b4
Chukwuemeka Ezeokoye <Chukwuemeka.Ezeokoye@Interswitchng.com>
2019-07-19 22:08:32 +0100

    Completed search component

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="bg-gray-400">
-    <div class="mx-auto lg:py-24 lg:px-16 px-4 py-4">
-      <div class="relative">
+  <div class="bg-gray-300 lg:h-64 h-32">
+    <div class="lg:py-24 lg:px-24 px-8 py-8">
+      <!-- Search Description -->
+      <div v-if="isSearch" class="flex justify-between">
+        <h1 class="text-gray-800 lg:pl-10 lg:text-4xl text-xl font-semibold">
+          {{ loading ? "Searching for " : "Search Results for "}}
+          <span
+            class="text-gray-500"
+          >"{{ keyword }}"</span>
+        </h1>
+        <svg
+          class="fill-current cursor-pointer"
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          viewBox="0 0 18 18"
+          @click="reset()"
+        >
+          <path
+            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+          />
+        </svg>
+      </div>
+
+      <!-- Search Input -->
+      <div v-else class="relative">
         <input
-          class="w w-full h-16 pl-16 rounded focus:outline-none text-xl text-black px-8 shadow-lg"
+          class="w-full h-16 pl-16 rounded focus:outline-none text-xl text-black px-8 shadow-lg"
           type="search"
-            placeholder="Search for photo"
+          placeholder="Search for photo"
+          v-model="keyword"
+          @keyup.enter="search()"
         />
         <svg
           class="absolute m-6 svg-icon fill-current top-0"
@@ -24,3 +49,31 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      keyword: "",
+      isSearch: false
+    };
+  },
+  methods: {
+    search() {
+      this.isSearch = true;
+      this.$emit("search", this.keyword)
+    },
+    reset() {
+      this.isSearch = false;
+      this.keyword = ""
+      this.$emit("reset")
+    }
+  }
+};
+</script>
--- src/container/Gallery.vue
+++ src/container/Gallery.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="content-center my-12 mx-auto px-4">
-    <SearchInput/>
+  <div class="content-center lg:my-12 my-4 mx-auto lg:px-12 px-4">
+    <SearchInput v-on:search="search($event)" v-on:reset="load()" />
   </div>
 </template>
 
 <script>
-import SearchInput from "../components/SearchInput"
+import SearchInput from "../components/SearchInput";
 export default {
-    components:{
-      SearchInput
+  components: {
+    SearchInput
+  },
+  methods: {
+    search(value) {
+      console.log(value);
+    },
+    load() {
+      console.log("load");
     }
-}
+  }
+};
 </script>

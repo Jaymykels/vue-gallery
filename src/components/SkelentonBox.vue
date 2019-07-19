24e298ae9d08b67af49af161e0093ab75b803a5c
Chukwuemeka Ezeokoye <Chukwuemeka.Ezeokoye@Interswitchng.com>
2019-07-19 23:45:18 +0100

    eslint formatting

--- src/components/SkelentonBox.vue
+++ src/components/SkelentonBox.vue
@@ -37,26 +37,32 @@ export default {
 
 <style>
 .loading {
-	 display: inline-block;
-	 position: relative;
-	 vertical-align: middle;
-	 overflow: hidden;
-	 background-color: #dddbdd;
+  display: inline-block;
+  position: relative;
+  vertical-align: middle;
+  overflow: hidden;
+  background-color: #dddbdd;
 }
- .loading::after {
-	 position: absolute;
-	 top: 0;
-	 right: 0;
-	 bottom: 0;
-	 left: 0;
-	 transform: translateX(-100%);
-	 background-image: linear-gradient(90deg, rgba(255, 255, 255, 0) 0, rgba(255, 255, 255, 0.2) 20%, rgba(255, 255, 255, 0.5) 50%, rgba(255, 255, 255, 0));
-	 animation: shimmer 5s infinite;
-	 content: "";
+.loading::after {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform: translateX(-100%);
+  background-image: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0) 0,
+    rgba(255, 255, 255, 0.2) 20%,
+    rgba(255, 255, 255, 0.5) 50%,
+    rgba(255, 255, 255, 0)
+  );
+  animation: shimmer 5s infinite;
+  content: "";
 }
- @keyframes shimmer {
-	 100% {
-		 transform: translateX(100%);
-	}
+@keyframes shimmer {
+  100% {
+    transform: translateX(100%);
+  }
 }
 </style>
